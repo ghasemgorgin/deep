a01b0937c30455f56f41ebd70f879df73d60fa2d
ghasem gorgin <ghasemgorgin74@gmail.com>
2025-07-31 05:11:13 +0330

    edit address

--- Exam 2/checkmycodeV1.py
+++ Exam 2/checkmycodeV1.py
@@ -1,5 +1,4 @@
-import pandas as pd
-import numpy as np
+3import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
